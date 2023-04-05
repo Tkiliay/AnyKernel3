e7620c65bfbebd01555b4333d352a03389088156
Tkiliay <sackclouths@outlook.com>
2023-04-05 16:36:52 +0800

    Rename

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Twilight by Tkiliay
+kernel.string=Twilight Kernel by Tkiliay
 do.devicecheck=1
 do.modules=0
 do.systemless=1
