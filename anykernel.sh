19021fed2c9783b1b609a83c6cd84bd66d94ad29
Tkiliay <sackclouths@gmail.com>
2024-11-03 10:24:16 +0800

    alioth: Write dtb and vendor_boot

--- anykernel.sh
+++ anykernel.sh
@@ -41,7 +41,7 @@ PATCH_VBMETA_FLAG=auto;
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
 # Override DTB
-#mv $home/dtb $home/split_img/;
+mv $home/dtb $home/split_img/;
 
 # init.tuna.rc
 backup_file init.tuna.rc;
@@ -103,17 +103,17 @@ write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_
 #} # end attributes
 
 # vendor_boot shell variables
-#BLOCK=vendor_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
+BLOCK=/dev/block/bootdevice/by-name/vendor_boot;
+IS_SLOT_DEVICE=1;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
 
 # reset for vendor_boot patching
-#reset_ak;
+reset_ak;
 
 # vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 ## end vendor_boot install
 
